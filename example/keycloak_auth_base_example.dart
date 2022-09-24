fec739ece29fd6f48163acfbc4c66d0b7e0439fa
Biblebreeze <manager@biblebreeze.com>
2022-09-24 08:35:47 -0500

    initial commit

---
+++ example/keycloak_auth_base_example.dart
@@ -0,0 +1,6 @@
+import 'package:keycloak_auth_base/keycloak_auth_base.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/keycloak_auth_base_test.dart
@@ -0,0 +1,16 @@
+import 'package:keycloak_auth_base/keycloak_auth_base.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
